cfebff06f5480aff31ada461805a8e1721939b11
Anna247147 <a67292032@gmail.com>
2024-07-08 14:59:12 +0300

    Add files via upload

--- module 5 1.py
+++ module 5 1.py
@@ -15,3 +15,16 @@ h1 = House('ЖК Горский', 18)
 h2 = House('Домик в деревне', 2)
 h1.go_to(5)
 h2.go_to(10)
+
+
+class Comedian:
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name} is {self.age}."
+new_comedian = Comedian("Анна", "Савина", "56")
+
+print(f"{new_comedian}")
